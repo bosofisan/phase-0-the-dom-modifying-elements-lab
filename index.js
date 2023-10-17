c78374ecc6edef781ebf20347d63b6bfce3ea335
lulu osofisan <boluosofisan@gmail.com>
2023-10-16 20:37:53 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainElement = document.querySelector("#main");
+mainElement.parentNode.removeChild(mainElement);
+
+const newHeader = document.createElement("h1");
+
+newHeader.textContent = "My new header is the champion";
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
